9b6d1d8be5cf71d988cb5ff2bb07b295be56c3ba
BunniyVenic <vanechkavery@gmail.com>
2024-09-24 09:24:05 +0700

    Add files via upload

---
+++ ьфшт.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import ttk
+import time
+import os
+canvas = Canvas( bg = 'white', height = 100, width = 100)
+canvas.pack()
+test = PhotoImage(file='пельмени1.png')
+time(5)
+root = Tk()
+root.title("METANIT.COM")
+root.geometry("250x200")
+#os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
