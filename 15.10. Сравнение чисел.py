7784fcb56ee0e6fb9fccc632566ca5c849d2e470
BunniyVenic <vanechkavery@gmail.com>
2024-10-15 11:50:52 +0700

    Update 15.10. Сравнение чисел.py

--- 15.10. Сравнение чисел.py
+++ 15.10. Сравнение чисел.py
@@ -2,4 +2,9 @@ first =int(input('Введите первое число: '))
 second = int(input('Введите второе число: '))
 third = int(input('Введите третье число: '))
 fourth = int(input('Введите четвертое число: '))
-print(max(first, second,third,fourth))
+def max_num():
+  num1 = max(first, second)
+  return num1
+max_num()
+num2=max(third,fourth)
+max(num1,num2)
