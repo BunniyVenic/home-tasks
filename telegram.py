892fe18caa522f63ec7e9213de1e2b704e122fe9
BunniyVenic <vanechkavery@gmail.com>
2025-01-14 11:25:11 +0700

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,24 +1,35 @@
 import telebot
+import time
 
-
-token='7664862192:AAE7OgDL7dsxCNHohrO7iOkAVVYsONDeMVk'
+token='My_token'
 bot=telebot.TeleBot(token)
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id,"https://www.youtube.com/watch?v=xvFZjo5PgG0")
-    bot.send_message(message.chat.id,"Здравствуйте! Перед началом работы, посмотрите наш туториал, отправленный выше. ")
-    bot.send_message(message.chat.id,"Для сортировки чисел, отправьте сообщением набор чисел разделенных точкой с запятой")
 
-@bot.message_handler(content_types=['document'])
-def handle_docs_photo(message):
 
-    chat_id = message.chat.id
-    file_info = bot.get_file(message.document.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    src = 'C:/Users/inna_/OneDrive/Рабочий стол/Веремеев Иван/pythonProject1/bots/files' + message.document.file_name;
-    with open(src, 'wb') as new_file:
-        new_file.write(downloaded_file)
-        bot.reply_to(message, "Пожалуй, я сохраню это")
+users = {}
 
-    bot.polling()
+
+@bot.message_handler(func=lambda message: True)
+def check_messages(message):
+    user_id = message.from_user.id
+
+    if user_id not in users:
+        users[user_id] = {
+            'message_count': 0,
+            'last_message_time': time.time()
+        }
+
+    current_time = time.time()
+    user = users[user_id]
+    user['message_count'] += 1
+    if current_time - user['last_message_time'] > 10:
+        user['message_count'] = 1
+        user['last_message_time'] = current_time
+
+    if user['message_count'] > 3:
+        bot.delete_message(message.chat.id, message.message_id)
+
+    user['last_message_time'] = current_time
+
+
+bot.polling()
